a9f0f407b186c8285bcbe62e403d623f4ee2f95f
Eduardo Franco <edufrancog17@gmail.com>
2024-01-10 00:31:36 +0100

    Primer commit del ejercicio 2, lo hicimos con Gabriel

---
+++ Ejercicio2/src/ejercicio2/Main.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+public class Main {
+	public static void main(String[] args) {
+		// Instanciamos la clase Cajero
+		Cajero cajero = new Cajero();
+		// Invocamos el método crear cuenta
+		cajero.crearCuenta("Gabriel Benítez","1234", "2020", 500);
+		cajero.crearCuenta("Eduardo Franco","5678", "0987", 500);
+		cajero.crearCuenta("Mario Cano","3448", "0564", 1);
+		
+		// Invocamos el método iniciarCajero para poner en marcha.
+		cajero.iniciarCajero();
+		
+	}
+}
